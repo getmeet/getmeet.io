4eb1369ce0200f0d21c88236489f34667be60663
rakesh singh <rakesh.singh2@go-mmt.com>
2021-01-03 12:35:20 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const googleOauth = {
-  client_id: "708389205872-3r9jdl4n97u91s0ibngtipqd9fsqj2l1.apps.googleusercontent.com",
+  client_id: "708389205872-u463nk0ukhd7mttt8g686dvnsorls3rd.apps.googleusercontent.com",
+  client_id_dev: "708389205872-3r9jdl4n97u91s0ibngtipqd9fsqj2l1.apps.googleusercontent.com",
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   redirect_uri: 'https://getmeet.io/login/google/callback',
 };
